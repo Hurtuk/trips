338e80abb1a5b65f3c0d57858859fb422b7dae68
Nicolas <niko.le@free.fr>
2019-09-26 00:29:05 +0200

    Bug

--- src/app/modals/trip-summary/trip-summary.component.ts
+++ src/app/modals/trip-summary/trip-summary.component.ts
@@ -57,7 +57,7 @@ export class TripSummaryComponent implements SummaryModal {
           // Creating maps
           setTimeout(() => {
             if (chapter.from && chapter.to) {
-              this.chartService.createTrip(this, 'map-chapter-' + chapter.number, null, [chapter.from, chapter.to], chapter.to.transport);
+              this.chartService.createTrip(this, 'map-chapter-' + chapter.number, null, [chapter.from, chapter.to]);
             }
           }, 0);
         });
